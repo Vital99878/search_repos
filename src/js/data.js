89ede58979d24da5ab6fb417313dc4ecbaa0a2f2
Vital <Lihoy84@yandex.ru>
2021-01-22 12:19:56 +0300

    created search func  and add to result list

--- src/js/data.js
+++ src/js/data.js
@@ -1,11 +1,14 @@
-export let input = document.querySelector( '.search__input' )
+export const input = document.querySelector( '.search__input' )
+export const result_list = document.querySelector( '.search__result-list' )
+export const fragment = document.createDocumentFragment();
+
 export const debounce = ( fn, debounceTime ) => {
   let timeout;
 
-  return function () {
-    let fn_call = () => {fn.apply (this, arguments)}
-    clearTimeout (timeout)
-    timeout = setTimeout (fn_call, debounceTime)
+  return function() {
+    let fn_call = () => {fn.apply( this, arguments )}
+    clearTimeout( timeout )
+    timeout = setTimeout( fn_call, debounceTime )
   }
 
 };
--- src/js/search.js
+++ src/js/search.js
@@ -1,25 +1,58 @@
-import { debounce, input } from './data'
+import { debounce, input, result_list, fragment } from './data'
 
 
 
 export function get_repos( cb, search_text ) {
+
   const xhr = new XMLHttpRequest()
 
-  xhr.open( 'get', `https://api.github.com/search/repositories?q=${ search_text }&per_page=5&sort=stars&order=desc` )
-  xhr.addEventListener( 'load', () => {
-    const response = JSON.parse( xhr.responseText )
-    let search_res = response.items.map( ( { name, owner, stargazers_count } ) => {
-      return { name: name, owner: owner.login, stars: stargazers_count }
+  if ( search_text === '' ) {
+    while ( result_list.firstChild ) {
+      result_list.removeChild( result_list.firstChild );
+    }
+
+  }
+  else {
+
+    while ( result_list.firstChild ) {
+      result_list.removeChild( result_list.firstChild );
+    }
+
+    xhr.open( 'get', `https://api.github.com/search/repositories?q=${ search_text }&per_page=5&sort=stars&order=desc` )
+
+    xhr.addEventListener( 'load', () => {
+      const response = JSON.parse( xhr.responseText )
+
+      let repos = response.items.map( ( item ) => {
+        return { name: item.name, owner: item.owner.login, stars: item.stargazers_count }
+      } )
+
+      cb( repos )
+
     } )
 
-    cb( search_res )
-  } )
-  xhr.addEventListener( 'error', ( e ) => console.log( 'error', e ) )
-  xhr.send()
+    xhr.addEventListener( 'error', ( e ) => {
+      cb( [ { name: 'no results', owner: 'no results', stars: 'no results' } ] )
+    } )
+
+    xhr.send()
+  }
+
 }
 
 get_repos = debounce( get_repos, 500 )
-input.addEventListener( 'keyup', () => get_repos( ( res ) => console.log( res ), input.value ) )
+
+input.addEventListener( 'keyup', () => get_repos( ( repos ) => {
+  console.log( repos )
+
+  repos.forEach( repo => {
+    let result_item = document.createElement( 'li' );
+    result_item.classList.add( 'search__result-item' )
+    result_item.textContent = repo.name;
+    fragment.appendChild( result_item )
+  } )
+  result_list.appendChild( fragment )
+}, input.value ) )
 
 
 
