a8ea92d10d4ab56e6dedf0f95d2732751d191d75
Vital <Lihoy84@yandex.ru>
2021-01-22 16:07:26 +0300

    completed

--- src/js/search.js
+++ src/js/search.js
@@ -1,60 +1,19 @@
-import { debounce, input, result_list, fragment, favorites__list } from './data'
+import { input, result_list, fragment, favorites__list, repos, cash_favorites, get_repos } from './data_for_search'
 
 
-let repos = [];
-
-export function get_repos( cb, search_text ) {
-
-  const xhr = new XMLHttpRequest()
-
-  if ( search_text === '' ) {
-    while ( result_list.firstChild ) {
-      result_list.removeChild( result_list.firstChild );
-    }
-
-  }
-  else {
-
-    while ( result_list.firstChild ) {
-      result_list.removeChild( result_list.firstChild );
-    }
-
-    xhr.open( 'get', `https://api.github.com/search/repositories?q=${ search_text }&per_page=5&sort=stars&order=desc` )
-
-    xhr.addEventListener( 'load', () => {
-      const response = JSON.parse( xhr.responseText )
-
-      repos = response.items.map( ( item ) => {
-        return { name: item.name, owner: item.owner.login, stars: item.stargazers_count }
-      } )
-
-      cb( repos )
-
-    } )
-
-    xhr.addEventListener( 'error', ( e ) => {
-      cb( [ { name: 'no results', owner: 'no results', stars: 'no results' } ] )
-    } )
-
-    xhr.send()
-  }
-
-}
-
-get_repos = debounce( get_repos, 500 )
-
-input.addEventListener( 'keyup', () => get_repos( ( repos ) => {
-  repos.forEach( ( repo, index ) => {
-    let result_item = document.createElement( 'li' );
-    result_item.classList.add( 'search__result-item' )
-    result_item.textContent = repo.name;
-    result_item.dataset.index = index
-    fragment.appendChild( result_item )
-  } )
-  result_list.appendChild( fragment )
-}, input.value ) )
 
 function add_item_to_favorites( el_index ) {
+
+  if ( cash_favorites.includes( repos[ el_index ].url ) ) {
+    alert( '!this repo already in favorites!' )
+    input.value = ''
+    while ( result_list.firstChild ) {
+      result_list.removeChild( result_list.firstChild );
+    }
+
+    return
+  }
+
   let favorite_item = document.createElement( 'li' )
   let favorite_item__content = document.createElement( 'div' )
   let favorite_item__p_name = document.createElement( 'p' )
@@ -71,12 +30,15 @@ function add_item_to_favorites( el_index ) {
   favorite_item__p_owner.textContent = `Owner: ${ repos[ el_index ].owner }`
   favorite_item__p_stars.textContent = `Stars: ${ repos[ el_index ].stars }`
 
+  cash_favorites.push( repos[ el_index ].url )
+
   favorite_item__content.appendChild( favorite_item__p_name )
   favorite_item__content.appendChild( favorite_item__p_owner )
   favorite_item__content.appendChild( favorite_item__p_stars )
 
   favorite_item.appendChild( favorite_item__content )
   favorite_item.appendChild( favorite_item__delete_button )
+  favorite_item.dataset.url = repos[ el_index ].url;
 
   favorites__list.prepend( favorite_item )
 
@@ -87,12 +49,37 @@ function add_item_to_favorites( el_index ) {
 }
 
 function delete_from_favorites( e ) {
+
   let del_item = e.target.parentNode;
+
   if ( del_item.className === 'favorites__item' ) {
+
+    let data_url = del_item.getAttribute( 'data-url' );
+
+    cash_favorites.map( ( item, index ) => {
+      if ( item === data_url ) {
+        delete cash_favorites[ index ];
+      }
+    } )
+
     del_item.remove()
+
   }
 
 }
+
+input.addEventListener( 'keyup', () => get_repos( ( repos ) => {
+  repos.forEach( ( repo, index ) => {
+    let result_item = document.createElement( 'li' );
+    result_item.classList.add( 'search__result-item' )
+    result_item.textContent = repo.name;
+    result_item.dataset.index = index
+    result_item.dataset.url = repo.url
+
+    fragment.appendChild( result_item )
+  } )
+  result_list.appendChild( fragment )
+}, input.value ) )
 
 result_list.addEventListener( 'click', ( e ) => {
   let el_index = e.target.getAttribute( 'data-index' );
