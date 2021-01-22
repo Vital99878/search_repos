f4286dceb8c029f391213f34c4827607ce968bd3
Vital <Lihoy84@yandex.ru>
2021-01-22 14:56:48 +0300

    without check: include in favorites

--- src/js/data.js
+++ src/js/data.js
@@ -1,5 +1,6 @@
 export const input = document.querySelector( '.search__input' )
 export const result_list = document.querySelector( '.search__result-list' )
+export const favorites__list = document.querySelector( '.favorites__list' )
 export const fragment = document.createDocumentFragment();
 
 export const debounce = ( fn, debounceTime ) => {
--- src/js/search.js
+++ src/js/search.js
@@ -1,6 +1,7 @@
-import { debounce, input, result_list, fragment } from './data'
+import { debounce, input, result_list, fragment, favorites__list } from './data'
 
 
+let repos = [];
 
 export function get_repos( cb, search_text ) {
 
@@ -23,7 +24,7 @@ export function get_repos( cb, search_text ) {
     xhr.addEventListener( 'load', () => {
       const response = JSON.parse( xhr.responseText )
 
-      let repos = response.items.map( ( item ) => {
+      repos = response.items.map( ( item ) => {
         return { name: item.name, owner: item.owner.login, stars: item.stargazers_count }
       } )
 
@@ -43,18 +44,63 @@ export function get_repos( cb, search_text ) {
 get_repos = debounce( get_repos, 500 )
 
 input.addEventListener( 'keyup', () => get_repos( ( repos ) => {
-  console.log( repos )
-
-  repos.forEach( repo => {
+  repos.forEach( ( repo, index ) => {
     let result_item = document.createElement( 'li' );
     result_item.classList.add( 'search__result-item' )
     result_item.textContent = repo.name;
+    result_item.dataset.index = index
     fragment.appendChild( result_item )
   } )
   result_list.appendChild( fragment )
 }, input.value ) )
 
+function add_item_to_favorites( el_index ) {
+  let favorite_item = document.createElement( 'li' )
+  let favorite_item__content = document.createElement( 'div' )
+  let favorite_item__p_name = document.createElement( 'p' )
+  let favorite_item__p_owner = document.createElement( 'p' )
+  let favorite_item__p_stars = document.createElement( 'p' )
+  let favorite_item__delete_button = document.createElement( 'button' )
 
+  favorite_item.classList.add( 'favorites__item' )
+  favorite_item__content.classList.add( 'favorites__content' )
+  favorite_item__delete_button.classList.add( 'favorites__close-button' )
+  favorite_item__delete_button.classList.add( 'close-button' )
+
+  favorite_item__p_name.textContent = `Name: ${ repos[ el_index ].name }`
+  favorite_item__p_owner.textContent = `Owner: ${ repos[ el_index ].owner }`
+  favorite_item__p_stars.textContent = `Stars: ${ repos[ el_index ].stars }`
+
+  favorite_item__content.appendChild( favorite_item__p_name )
+  favorite_item__content.appendChild( favorite_item__p_owner )
+  favorite_item__content.appendChild( favorite_item__p_stars )
+
+  favorite_item.appendChild( favorite_item__content )
+  favorite_item.appendChild( favorite_item__delete_button )
+
+  favorites__list.prepend( favorite_item )
+
+  input.value = ''
+  while ( result_list.firstChild ) {
+    result_list.removeChild( result_list.firstChild );
+  }
+}
+
+function delete_from_favorites( e ) {
+  let del_item = e.target.parentNode;
+  if ( del_item.className === 'favorites__item' ) {
+    del_item.remove()
+  }
+
+}
+
+result_list.addEventListener( 'click', ( e ) => {
+  let el_index = e.target.getAttribute( 'data-index' );
+  add_item_to_favorites( el_index )
+  favorites__list.addEventListener( 'click', ( e ) => {
+    delete_from_favorites( e )
+  } )
+} )
 
 
 
